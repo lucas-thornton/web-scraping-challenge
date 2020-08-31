88f5791500432ba69d770cbed78aa842b601a595
lucas-thornton <le.thornton644@gmail.com>
2020-08-31 15:58:43 +0800

    update

--- Mission_to Mars /app.py
+++ Mission_to Mars /app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import mission_to_mars
+import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app?ssl=true&ssl_cert_reqs=CERT_NONE")
 
 
 # Route to render index.html template using data from Mongo
@@ -15,9 +15,10 @@ def home():
 
     # Find one record of data from the mongo database
     # @TODO: YOUR CODE HERE!
+    mars_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", mars=mars_data)
 
 
 # Route that will trigger the scrape function
@@ -26,9 +27,11 @@ def scrape():
 
     # Run the scrape function and save the results to a variable
     # @TODO: YOUR CODE HERE!
+    page_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
     # @TODO: YOUR CODE HERE!
+    mongo.db.collection.update({}, page_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
---
+++ Mission_to Mars /scrape_mars.py
@@ -0,0 +1,87 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pandas as pd
+import requests
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {'executable_path':ChromeDriverManager().install()}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape():
+    browser = init_browser()
+
+# Latest News
+    url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
+    browser.visit(url)
+    time.sleep(1)
+    html = browser.html
+    soup = bs(html, "html.parser")
+    title = soup.find(class_='content_title').a
+    latest_title = title.text.strip()
+    para = soup.find(class_='article_teaser_body').text
+    latest_para = para.text
+# Full Image
+    url_mars = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url_mars)
+    browser.find_by_id('full_image').click()
+    time.sleep(1)
+    html = browser.html
+    soup = bs(html, "html.parser")  
+    relative_image_path = soup.find_all('img')[3]['src']
+    base_url = 'https://www.jpl.nasa.gov'
+    full_image_url = base_url + relative_image_path
+# Table
+    url_table = 'https://space-facts.com/mars/'
+    tables = pd.read_html(url_table)
+    mars_facts = tables[0]
+    html_table = mars_facts.to_html()
+    html_table.replace('\n', '')    
+    mars_facts.to_html('table.html')
+# Hemispheres
+    astr_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(astr_url)
+    html = browser.html
+    soup = bs(html, "html.parser")
+    x = soup.find_all('h3')
+    y = soup.find_all(class_='description')
+    mars_url_base = 'https://astrogeology.usgs.gov'
+    hemisphere_list = []
+
+    for i in range(0, 4):
+        title = x[i].text
+        img_url = mars_url_base + y[i].a['href']
+        browser.visit(img_url)
+        html = browser.html
+        soup = bs(html, "html.parser") 
+        image_path = soup.find('li').a['href']
+        browser.visit(astr_url)
+        html = browser.html
+        soup = bs(html, "html.parser")
+        
+        hemisphere = {
+                'title': title,
+                'img_url': image_path,
+            }
+        hemisphere_list.append(hemisphere)
+
+    mars_data = {
+        "Latest_News_Title":latest_title,
+        "Latest_News_Preview":latest_para,
+        "Full_Image": full_image_url,
+        "Hemisphere_1": hemisphere_list[0],
+        "Hemisphere_2": hemisphere_list[1],
+        "Hemisphere_3": hemisphere_list[2],
+        "Hemisphere_4": hemisphere_list[3]
+    
+    }
+
+    browser.quit()
+
+    # Return results
+    return mars_data
+
+
